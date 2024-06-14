67e0643ad843e07e24d5b3fba3fc7cbe63bbeb59
Gianluca Zicca <gianluca.zicca@bestit.de>
2024-06-14 13:21:46 +0200

    main initial

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-blue-400">Protimer - Check your times</h1>
+  <UButton>Test</UButton>
+</template>
+
+<style scoped>
+
+</style>
